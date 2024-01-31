ab80fd0293d233cf32b0dd778817751c14bbfbbd
fotweni <kolesnikovv028@gmail.com>
2024-01-31 21:51:44 +0000

    week02: implemented test_training

--- week02_management_and_testing/homework/modeling/training.py
+++ week02_management_and_testing/homework/modeling/training.py
@@ -16,7 +16,7 @@ def train_step(model: DiffusionModel, inputs: torch.Tensor, optimizer: Optimizer
     return loss
 
 
-def train_epoch(model: DiffusionModel, dataloader: DataLoader, optimizer: Optimizer, device: str):
+def train_epoch(model: DiffusionModel, dataloader: DataLoader, optimizer: Optimizer, device: str) -> float:
     model.train()
     pbar = tqdm(dataloader)
     loss_ema = None
@@ -24,6 +24,8 @@ def train_epoch(model: DiffusionModel, dataloader: DataLoader, optimizer: Optimi
         train_loss = train_step(model, x, optimizer, device)
         loss_ema = train_loss if loss_ema is None else 0.9 * loss_ema + 0.1 * train_loss
         pbar.set_description(f"loss: {loss_ema:.4f}")
+
+    return loss_ema
 
 
 def generate_samples(model: DiffusionModel, device: str, path: str):
--- week02_management_and_testing/homework/tests/test_pipeline.py
+++ week02_management_and_testing/homework/tests/test_pipeline.py
@@ -1,13 +1,19 @@
+from pathlib import Path
 import pytest
 import torch
 from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, ToTensor, Normalize
 from torchvision.datasets import CIFAR10
+from torchvision import transforms
 
 from modeling.diffusion import DiffusionModel
-from modeling.training import train_step
+from modeling.training import train_step, generate_samples, train_epoch
 from modeling.unet import UnetModel
 
+
+@pytest.fixture
+def set_seed():
+    torch.manual_seed(0)
 
 @pytest.fixture
 def train_dataset():
@@ -22,12 +28,13 @@ def train_dataset():
 
 
 @pytest.mark.parametrize(["device"], [["cpu"], ["cuda"]])
-def test_train_on_one_batch(device, train_dataset):
+def test_train_on_one_batch(device, train_dataset, set_seed):
     # note: you should not need to increase the threshold or change the hyperparameters
     ddpm = DiffusionModel(
         eps_model=UnetModel(3, 3, hidden_size=32),
         betas=(1e-4, 0.02),
         num_timesteps=1000,
+        device=device,
     )
     ddpm.to(device)
 
@@ -41,6 +48,50 @@ def test_train_on_one_batch(device, train_dataset):
     assert loss < 0.5
 
 
-def test_training():
+@pytest.mark.parametrize(
+        'device,num_epochs,hidden_size,expected_loss',
+        [
+            ('cpu', 1, 8, 1.03),
+            ('cuda', 2, 16, 0.77)
+        ]
+)
+def test_training(
+    device,
+    num_epochs,
+    hidden_size,
+    expected_loss,
+    set_seed,
+    tmp_path
+):
     # note: implement and test a complete training procedure (including sampling)
-    pass
+    ddpm = DiffusionModel(
+        eps_model=UnetModel(3, 3, hidden_size=hidden_size),
+        betas=(1e-4, 0.02),
+        num_timesteps=1000,
+        device=device,
+    )
+    ddpm.to(device)
+
+    train_transforms = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))]
+    )
+
+    dataset = CIFAR10(
+        "cifar10",
+        train=True,
+        download=True,
+        transform=train_transforms,
+    )
+
+    dataloader = DataLoader(dataset, batch_size=128, num_workers=4, shuffle=True)
+    optim = torch.optim.Adam(ddpm.parameters(), lr=1e-5)
+
+    loss = None
+    for _ in range(num_epochs):
+        loss = train_epoch(ddpm, dataloader, optim, device)
+        samples_path = tmp_path/'{i:02d}.png'
+        generate_samples(ddpm, device, samples_path)
+
+        assert samples_path.exists()
+
+    assert abs(loss.detach().item() - expected_loss) < 1e-3
