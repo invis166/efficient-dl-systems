618b6a5e101db0e08d902e6b73889219357e94e3
Semen Kolesnikov <sem.kolesnikov@tinkoff.ru>
2024-03-03 22:55:09 +0500

    solution for syncbn

--- week05_data_parallel/homework/syncbn.py
+++ week05_data_parallel/homework/syncbn.py
@@ -17,15 +17,51 @@ class sync_batch_norm(Function):
     """
 
     @staticmethod
-    def forward(ctx, input, running_mean, running_std, eps: float, momentum: float):
+    def forward(ctx, input, running_mean, running_std, eps: float, momentum: float, world_size: int):
         # Compute statistics, sync statistics, apply them to the input
         # Also, store relevant quantities to be used on the backward pass with `ctx.save_for_backward`
-        pass
+        batch_size = input.shape[0]
+        global_batch_size = batch_size * world_size
+
+        # getting global sum of elements and global sum of squared elements
+        input_squared = input * input
+        input_with_squared = torch.concat([input, input_squared], dim=-1)
+        dist.all_reduce(input_with_squared, dist.ReduceOp.SUM)
+        input_reduced, input_squared_reduced = torch.chunk(input_with_squared, chunks=2, dim=-1)
+
+        # Var[X] = E[X^2] - E[X]^2
+        global_mean = input_reduced.sum(axis=0) / global_batch_size
+        global_std = torch.sqrt(input_squared_reduced.sum(axis=0) / global_batch_size - global_mean**2 + eps)
+
+        # updating running statistics
+        running_mean_new = (1 - momentum) * global_mean + momentum * running_mean
+        running_std_new = (1 - momentum) * global_std + momentum * running_std
+
+        # saving statistics for the backward pass
+        ctx.save_for_backward(global_mean, global_std, input, torch.tensor(global_batch_size))
+
+        return (input - global_mean) / (global_std), running_mean_new, running_std_new
 
     @staticmethod
-    def backward(ctx, grad_output):
+    def backward(ctx, grad_output, running_mean, running_std):
         # don't forget to return a tuple of gradients wrt all arguments of `forward`!
-        pass
+        global_mean, global_std, input, global_batch_size = ctx.saved_tensors
+
+        grad_reduced = torch.clone(grad_output)
+        grad_prod_inp_minus_mu_reduced = grad_reduced * (input - global_mean)
+        combined = torch.cat([grad_reduced, grad_prod_inp_minus_mu_reduced], dim=-1)
+        dist.all_reduce(combined, dist.ReduceOp.SUM)
+        grad_reduced, grad_prod_inp_minus_mu_reduced = torch.chunk(combined, 2, dim=-1)
+
+        d_act_d_inp = 1 / global_std
+
+        d_loss_d_mu = -1 / global_std * grad_reduced.sum(axis=0)
+        d_mu_d_inp = 1 / global_batch_size
+
+        d_loss_d_sigma = -1 / global_std**2 * grad_prod_inp_minus_mu_reduced.sum(axis=0)
+        d_sigma_d_inp = 1 / global_std * (input - global_mean) / global_batch_size
+
+        return grad_output * d_act_d_inp + d_loss_d_mu * d_mu_d_inp + d_loss_d_sigma * d_sigma_d_inp, None, None, None, None, None
 
 
 class SyncBatchNorm(_BatchNorm):
@@ -48,6 +84,15 @@ class SyncBatchNorm(_BatchNorm):
         self.running_mean = torch.zeros((num_features,))
         self.running_std = torch.ones((num_features,))
 
+        self._world_size = dist.get_world_size()
+        self._bn = sync_batch_norm.apply
+
     def forward(self, input: torch.Tensor) -> torch.Tensor:
         # You will probably need to use `sync_batch_norm` from above
-        pass
+        normalized, running_mean_new, running_std_new = self._bn(
+            input, self.running_mean, self.running_std, self.eps, self.momentum, self._world_size
+        )
+        self.running_mean = running_mean_new
+        self.running_std = running_std_new
+
+        return normalized
