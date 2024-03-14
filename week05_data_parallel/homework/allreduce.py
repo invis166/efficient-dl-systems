42dae3790ac0ac691045fb4bcfe1093304b43bdd
fotweni <kolesnikovv028@gmail.com>
2024-03-14 22:35:47 +0000

    all reduce and final report for week05

--- week05_data_parallel/homework/allreduce.py
+++ week05_data_parallel/homework/allreduce.py
@@ -1,17 +1,26 @@
+import resource
 import os
 import random
 
 import torch
 import torch.distributed as dist
-from torch.multiprocessing import Process
+import time
+import torch.multiprocessing as mp
 
 
-def init_process(rank, size, fn, master_port, backend="gloo"):
+
+def init_process(rank, world_size, tensor_size, fn, master_port, backend="gloo"):
     """Initialize the distributed environment."""
     os.environ["MASTER_ADDR"] = "127.0.0.1"
     os.environ["MASTER_PORT"] = str(master_port)
-    dist.init_process_group(backend, rank=rank, world_size=size)
-    fn(rank, size)
+    dist.init_process_group(backend, rank=rank, world_size=world_size)
+
+    start_time = time.perf_counter()
+    initial_tensor, reduced = fn(rank, world_size, tensor_size)
+    end_time = time.perf_counter()
+    max_memory_usage = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+
+    return initial_tensor, reduced, end_time - start_time, max_memory_usage
 
 
 def butterfly_allreduce(send, rank, size):
@@ -69,26 +78,128 @@ def ring_allreduce(send, rank, size):
         rank: Current process rank (in a range from 0 to size)
         size: Number of workers
     """
-    pass
+    chunks = list(torch.chunk(send, size))
+    recv_buffer = torch.empty((max(map(len, chunks)),), dtype=torch.float)
+
+    send_chunk = rank
+    recv_chunk = (send_chunk - 1) % size
+    for round in range(size - 1):
+        send_future = dist.isend(chunks[send_chunk], (rank + 1) % size)
+        recv_future = dist.irecv(recv_buffer, (rank - 1) % size)
+        recv_future.wait()
+        send_future.wait()
+        chunks[recv_chunk] += recv_buffer
+
+        send_chunk, recv_chunk = recv_chunk, (recv_chunk - 1) % size
+
+    send_chunk = (rank + 1) % size
+    recv_chunk = (send_chunk - 1) % size
+    for round in range(size - 1):
+        send_future = dist.isend(chunks[send_chunk], (rank + 1) % size)
+        recv_future = dist.irecv(recv_buffer, (rank - 1) % size)
+        recv_future.wait()
+        send_future.wait()
+        chunks[recv_chunk].copy_(recv_buffer)
+
+        send_chunk, recv_chunk = recv_chunk, (recv_chunk - 1) % size
+
+    send.copy_(torch.hstack(chunks) / size)
 
 
-def run_butterfly_allreduce(rank, size):
+def run_butterfly_allreduce(rank, world_size, tensor_size):
     """Simple point-to-point communication."""
     torch.manual_seed(rank)
-    tensor = torch.randn((size,), dtype=torch.float)
-    print("Rank ", rank, " has data ", tensor)
-    butterfly_allreduce(tensor, rank, size)
-    print("Rank ", rank, " has data ", tensor)
+    tensor = torch.randn((tensor_size,), dtype=torch.float)
+    initial_tensor = torch.clone(tensor)
+    butterfly_allreduce(tensor, rank, world_size)
+
+    return initial_tensor, tensor
+
+
+def run_ring_allreduce(rank, world_size, tensor_size):
+    """Simple point-to-point communication."""
+    torch.manual_seed(rank)
+    tensor = torch.randn((tensor_size,), dtype=torch.float)
+    initial_tensor = torch.clone(tensor)
+    ring_allreduce(tensor, rank, world_size)
+
+    return initial_tensor, tensor
+
+
+def run_torch_allreduce(rank, world_size, tensor_size):
+    """Simple point-to-point communication."""
+    torch.manual_seed(rank)
+    tensor = torch.randn((tensor_size,), dtype=torch.float)
+    initial_tensor = torch.clone(tensor)
+    dist.all_reduce(tensor, dist.ReduceOp.SUM)
+
+    return initial_tensor, tensor / world_size
+
+
+def test_butterfly():
+    world_sizes = [1, 2, 4, 8, 16, 32]
+    tensor_sizes = [4, 8, 16, 64, 128, 512]
+
+    for world_size, tensor_size in zip(world_sizes, tensor_sizes):
+        print(f'testing {world_size=} {tensor_size=}')
+        gt, reduced, *_ = _run_allreduce(world_size, tensor_size, run_ring_allreduce)
+
+        assert torch.allclose(gt, reduced, rtol=1e-4)
+
+
+def _run_allreduce(world_size, tensor_size, fn):
+    port = random.randint(25000, 30000)
+    ctx = mp.get_context('spawn')
+    with ctx.Pool(processes=world_size) as pool:
+        results = pool.starmap(
+            init_process,
+            [(rank, world_size, tensor_size, fn, port) for rank in range(world_size)]
+        )
+
+    gt = torch.mean(torch.vstack([res[0] for res in results]), axis=0)
+    reduced_tensor = results[0][1]
+    max_time_consumed = max(res[2] for res in results)
+    max_memory_usage = max(res[3] for res in results)
+
+    return gt, reduced_tensor, max_time_consumed, max_memory_usage
+
+
+def _run_benchmark(world_size, tensor_size, fn, n=10):
+    mean_max_time = 0
+    mean_max_memory = 0
+    for _ in range(n):
+        gt, reduced, max_time_consumed, max_memory = _run_allreduce(world_size, tensor_size, fn)
+        mean_max_time += max_time_consumed
+        mean_max_memory += max_memory
+
+    return gt, reduced, mean_max_time / n, mean_max_memory / n
+
+
+def compare_custom():
+    world_sizes = [1, 2, 4, 8, 16, 32]
+    tensor_sizes = [1, 2, 4, 8, 16, 32]
+
+    for world_size, tensor_size in zip(world_sizes, tensor_sizes):
+        gt1, ring_reduced, ring_max_time_consumed, ring_max_memory = _run_benchmark(world_size, tensor_size, run_ring_allreduce)
+        gt2, butterfly_reduced, butterfly_max_time_consumed, butterfly_max_memory = _run_benchmark(world_size, tensor_size, run_butterfly_allreduce)
+        diff_ring = torch.sum((ring_reduced - gt1)**2)
+        diff_butterfly = torch.sum((butterfly_reduced - gt2)**2)
+        print(f'{world_size=} {tensor_size=} | {ring_max_time_consumed=:.3f}, {butterfly_max_time_consumed=:.3f}, {ring_max_memory=}, {butterfly_max_memory=} {diff_ring=}, {diff_butterfly=}')
+
+
+def compare_with_torch():
+    world_sizes = [1, 2, 4, 8, 16]
+    tensor_sizes = [1024, 2048, 4096, 16384, 131072]
+
+    for world_size, tensor_size in zip(world_sizes, tensor_sizes):
+        gt1, ring_reduced, ring_max_time_consumed, ring_max_memory = _run_benchmark(world_size, tensor_size, run_ring_allreduce)
+        gt2, torch_reduced, torch_max_time_consumed, torch_max_memory = _run_benchmark(world_size, tensor_size, run_torch_allreduce)
+        diff_ring = torch.sum((ring_reduced - gt1)**2)
+        diff_torch = torch.sum((torch_reduced - gt2)**2)
+        print(f'{world_size=} {tensor_size=} | {ring_max_time_consumed=:.3f}, {torch_max_time_consumed=:.3f}, {ring_max_memory=}, {torch_max_memory=} {diff_ring=}, {diff_torch=}')
 
 
 if __name__ == "__main__":
-    size = 5
-    processes = []
-    port = random.randint(25000, 30000)
-    for rank in range(size):
-        p = Process(target=init_process, args=(rank, size, run_butterfly_allreduce, port))
-        p.start()
-        processes.append(p)
-
-    for p in processes:
-        p.join()
+    test_butterfly()
+    compare_custom()
+    compare_with_torch()
--- week05_data_parallel/homework/report.md
+++ week05_data_parallel/homework/report.md
@@ -58,3 +58,28 @@ Selfmade BN
 to reproduce: `CUDA_VISIBLE_DEVICES=X,Y python syncbn_benchmark.py`
 
 in general, custom implementation is turned out to be faster but is has larger memory consumption.
+
+# Ring all reduce
+
+Comprasion with given butterfly all reduce
+
+| world_size | tensor_size | ring_max_time_consumed | butterfly_max_time_consumed | ring_max_memory | butterfly_max_memory | diff_ring | diff_butterfly |
+|------------|-------------|-----------------------|-----------------------------|----------------|----------------------|-----------|------------|
+| 1 | 1 | 0.017 | 0.012 | 379424.0 | 379424.0 | 0.0000 | 0.0000 |
+| 2 | 2 | 0.024 | 0.026 | 380787.2 | 380966.4 | 0.0000 | 0.0000 |
+| 4 | 4 | 0.026 | 0.027 | 380989.6 | 380996.0 | 2.3592e-16 | 0.0000 |
+| 8 | 8 | 0.034 | 0.031 | 382106.8 | 382218.8 | 2.5535e-15 | 1.6653e-15 |
+| 16 | 16 | 0.043 | 0.036 | 382266.0 | 382289.6 | 1.8666e-14 | 5.0515e-15 |
+| 32 | 32 | 0.057 | 0.048 | 382398.4 | 382439.6 | 3.7367e-14 | 1.6990e-14 |
+
+Comprasion with pytorch all reduce
+
+| world_size | tensor_size | ring_max_time_consumed | torch_max_time_consumed | ring_max_memory | torch_max_memory | diff_ring | diff_torch |
+|------------|-------------|-----------------------|------------------------|----------------|-----------------|-----------|------------|
+| 1 | 1024 | 0.015 | 0.020 | 382460.0 | 382460.0 | 0.0000 | 0.0000 |
+| 2 | 2048 | 0.028 | 0.025 | 382460.0 | 382460.0 | 0.0000 | 0.0000 |
+| 4 | 4096 | 0.024 | 0.026 | 382523.2 | 382586.4 | 2.4260e-12 | 2.7529e-12 |
+| 8 | 16384 | 0.030 | 0.032 | 384578.4 | 385378.8 | 1.0071e-11 | 1.1579e-11 |
+| 16 | 131072 | 0.359 | 0.258 | 405353.2 | 410945.6 | 7.6576e-11 | 8.7605e-11 |
+
+to reproduce: `python allredce.py`
